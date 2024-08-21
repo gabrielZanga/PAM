2a0ece0a4d5e21cf1922d99a3dd528f0fa798282
gah__vi <138242215+gabrielZanga@users.noreply.github.com>
2024-08-21 08:13:12 -0300

    Add files via upload

---
+++ glapp/App.tsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import ligado from './assets/pictures/lampadaacesa.png';
+import desligado from './assets/pictures/lampadaapag.png';
+
+export default function App(){
+
+  const [isActive, setisActive] = useState(false);
+
+  function handleSymbol(){
+      setisActive((oldValue : boolean) =>{
+        return !oldValue;
+      })
+ }
+         
+  return (
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={handleSymbol}>
+     <Image source={isActive ? ligado : desligado}></Image>
+     </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerOn: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  containerOff: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
